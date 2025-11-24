8ce3c46e83e461606941ac8284ee797b41836747
yenrahj <jharney@allcampus.com>
2025-11-24 12:00:32 -0600

    Update warm-up-engagement.js

--- api/cron/warm-up-engagement.js
+++ api/cron/warm-up-engagement.js
@@ -36,7 +36,7 @@ module.exports = async (req, res) => {
 
     for (const contact of contacts) {
       try {
-        const linkedinUrl = contact.properties.linkedin_url;
+        const linkedinUrl = contact.properties.hs_linkedin_url;
         const warmUpCount = parseInt(contact.properties.linkedin_warm_up_count || '0');
         
         if (!linkedinUrl) {
